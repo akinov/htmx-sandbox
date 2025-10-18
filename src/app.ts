3d92605722454dd5bfdecf5c9c9c851b058d7544
akinov <ayumoto10@gmail.com>
2025-10-18 17:01:25 +0900

    chore: 不要なファイルとコメントの削除
    - src/routes/clicked.ts を削除
    - src/routes/index.ts を削除
    - src/app.ts からテスト用コメントを削除

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,6 @@ import clickedRouter from './routes/clicked/index.js'
 import formRouter from './routes/form/index.js'
 import { securityHeaders, csrfProtection, corsConfig } from './middleware/security.js'
 
-// テスト用のコメント
-
 const app = new Hono()
 
 // セキュリティミドルウェアの適用
